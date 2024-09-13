b365a27760ebb7c0a9c4f9824d30de83b896e086
Dishu Kumar <163343741+Jaarvis05@users.noreply.github.com>
2024-09-13 14:29:47 +0530

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,51 @@
+//Whenever we fetch API we need URL & API Key....
+const apiKey= "6ffb4df5a3ed187a6d06072840acb02d";
+const apiURL= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+async function checkweather(city){
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    if(response.status == 404){
+        document.querySelector(".error").style.display= "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        var data = await response.json();   // data converted into jason file & store in data....
+    console.log(data);
+
+document.querySelector(".city").innerHTML= data.name;
+document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + "°C";
+document.querySelector(".humidity").innerHTML= data.main.humidity + "%";
+document.querySelector(".Wind").innerHTML= data.wind.speed + "Km/h";
+
+if(data.weather[0].main == "Clouds"){
+    weatherIcon.src= "images/clouds.png";
+}
+else if(data.weather[0].main == "Clear"){
+    weatherIcon.src= "images/clear.png";
+}
+else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src= "images/drizzle.png";
+}
+else if(data.weather[0].main == "Mist"){
+    weatherIcon.src= "images/mist.png";
+}
+else if(data.weather[0].main == "Rain"){
+    weatherIcon.src= "images/rain.png";
+}
+else if(data.weather[0].main == "Snow"){
+    weatherIcon.src= "images/snow.png";
+}
+document.querySelector(".weather").style.display = "block"; 
+document.querySelector(".error").style.display= "none";
+    //here when data find then show weather otherwise block display....
+}
+    }
+
+searchBtn.addEventListener("click",()=>{
+    checkweather(searchBox.value);
+})
